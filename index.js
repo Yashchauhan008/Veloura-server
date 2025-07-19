861d36777ec410519b3a6e0951a594e9be7bfc65
Agravatkirtan <kirtanagravat7@gmail.com>
2025-07-19 09:12:41 +0530

    checking done

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 const MONGODB_URI = process.env.MONGODB_URI;
 
-console.log("i am here");
-
-//configuration
-
 app.use(
   cors({
     origin: true,
